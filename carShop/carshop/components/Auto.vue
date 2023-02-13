95f87d3738da549ab98d8fb8a54cb1bede24cefb
Akvirac <juanarielvivian@gmail.com>
2023-02-13 12:02:14 -0300

    avancescarshop4

--- carShop/carshop/components/Auto.vue
+++ carShop/carshop/components/Auto.vue
@@ -1,10 +1,22 @@
 <script setup>
+import { autos } from '~/data'
+
 defineProps({
 	auto: {
 		type: Object,
 		required: true,
 	},
 })
+
+const isModalShow = ref(false)
+
+function openModal() {
+	isModalShow.value = true
+}
+
+function closeModal() {
+	isModalShow.value = false
+}
 </script>
 
 <template>
@@ -33,7 +45,32 @@ defineProps({
 					</p>
 				</div>
 			</div>
-			<Boton class="mt-2">Ofertar</Boton>
 		</NuxtLink>
+		<Boton @click="openModal">Ofertar</Boton>
 	</Contenedor>
+	<div
+		v-if="isModalShow"
+		class="main-modal fixed w-full inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
+	>
+		<div
+			class="shadow border border-blue-500 shadow-lg modal-container bg-gray-100 w-4/12 md:max-w-11/12 mx-auto rounded-xl shadow-lg z-50 overflow-y-auto"
+		>
+			<div class="modal-content py-4 px-6">
+				<div class="flex justify-end">
+					<Boton class="" @click="closeModal">X</Boton>
+				</div>
+				<div class="flex justify-center pb-4">
+					<p class="font-bold">
+						{{ auto.marca }}/{{ auto.modelo }}/{{ auto.año }}
+					</p>
+					<p></p>
+				</div>
+				<Input text="Valor de la oferta" type="text" />
+				<Input text="Numero de Contacto" type="text" />
+				<div class="flex justify-center mt-4">
+					<Boton @click="$emit('submit', auto)">Confirmar Oferta</Boton>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
--- carShop/carshop/components/ListaAutos.vue
+++ carShop/carshop/components/ListaAutos.vue
@@ -2,6 +2,7 @@
 //import de autos desde data.
 import { autos } from '~/data'
 </script>
+
 <template>
 	<!-- Acomodamos el template en grillas de 3 columnas. -->
 	<ul class="grid grid-cols-3 gap-4 place-items-center mx-5 mt-5">
